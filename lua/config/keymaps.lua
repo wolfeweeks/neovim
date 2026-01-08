1b3d6647730baee87671ef5920ab350f48e611f9
wolfeweeks <wolfesevenweeks@gmail.com>
2026-01-08 11:15:11 -0600

    add keymap to yank entire buffer to clipboard

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Yank entire buffer to system clipboard
+vim.keymap.set("n", "<leader>ya", ':%y+<CR>', { desc = "Yank all to clipboard" })
